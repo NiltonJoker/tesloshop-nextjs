790575b2a73a17c3d146b510fca0f834f0f914df
Nilton <nilton242009@hotmail.com>
2022-05-13 01:55:03 -0500

    añadiendo endpoints a la api

---
+++ pages/api/seed.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db, seedDatabase } from '../../database'
+import { Product } from '../../models';
+
+type Data = {
+  message: string
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>){
+  switch (req.method) {
+    case "GET":
+      return insertSeedData(req, res);
+  
+    default:
+      return res.status(400).json({ message: "Metodo no existe" })
+  }
+}
+
+async function insertSeedData(req: NextApiRequest, res: NextApiResponse<Data>) {
+  
+  if( process.env.NODE_ENV === 'production' )
+    return res.status(401).json({ message: 'No tiene acceso a este servicio' });
+  
+
+  await db.connect();
+
+  await Product.deleteMany();
+  await Product.insertMany( seedDatabase.initialData.products )
+
+  await db.disconnect();
+
+  res.status(200).json({ message: 'Proceso realizado correctamente' })
+
+}
