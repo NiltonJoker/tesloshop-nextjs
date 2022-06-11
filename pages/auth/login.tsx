e88e1beaab1247beaea9b3d06ece6ea57b303b7f
Nilton <nilton242009@hotmail.com>
2022-06-11 17:32:11 -0500

    Añadiendo AuthContext

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,49 +1,116 @@
 import NextLink from "next/link";
-import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Box, Button, Chip, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../../components/layouts";
+import { useForm } from "react-hook-form";
+import { validations } from "../../utils";
+import { ErrorOutline } from "@mui/icons-material";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context";
+import { useRouter } from "next/router";
+
+type FormData = {
+  email: string,
+  password: string
+}
+
 
 const LoginPage = () => {
+
+  const router = useRouter();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const [showError, setShowError] = useState(false);
+  const { loginUser } = useContext(AuthContext)
+
+
+  const onLoginUser = async ({ email, password }: FormData) => {
+    setShowError(false);
+
+    const isValidLogin = await loginUser(email, password);
+
+    if (!isValidLogin) {
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+      return;
+    }
+
+    // TODO: Navegar a la pantalla anterior o dirigir al home
+    router.replace('/');
+  };
+  
+
   return (
     <AuthLayout title={"Ingresar"}>
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Iniciar Sesion
-            </Typography>
-          </Grid>
+      <form onSubmit={ handleSubmit(onLoginUser) } noValidate >
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Iniciar Sesion
+              </Typography>
 
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
+              {showError && (
+                <Chip
+                  label="No reconocemos ese usuario / contraseña"
+                  color="error"
+                  icon={<ErrorOutline/>}
+                  className="fadeIn"
+                />
+              )}
 
-          <Grid item xs={12}>
-            <TextField
-              label="Contaseña"
-              variant="filled"
-              type="password"
-              fullWidth
-            />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField 
+                type="email"
+                label="Correo" 
+                variant="filled" 
+                fullWidth 
+                {
+                  ...register('email',{
+                    required: 'Este campo es requerido',
+                    validate: validations.isEmail
+                  })
+                }
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contaseña"
+                variant="filled"
+                type="password"
+                fullWidth
+                {
+                  ...register('password',{
+                    required: 'Este campo es requerido',
+                    minLength: { value: 6, message: 'Minimo 6 caracteres' }
+                  })
+                }
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                color="secondary"
+                className="circular-btn"
+                type="submit"
+                size="large"
+                fullWidth
+              >
+                Ingresar
+              </Button>
+            </Grid>
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href={`/auth/register`} passHref>
+                <Link underline="always">¿No tienes cuenta?</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Ingresar
-            </Button>
-          </Grid>
-
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href={`/auth/register`} passHref>
-              <Link underline="always">¿No tienes cuenta?</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,53 +1,142 @@
+import { useState, useContext } from "react";
 import NextLink from "next/link";
-import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+  Chip,
+} from "@mui/material";
 import { AuthLayout } from "../../components/layouts";
+import { useForm } from "react-hook-form";
+import { validations } from "../../utils";
+import { ErrorOutline } from "@mui/icons-material";
+import { useRouter } from "next/router";
+import { AuthContext } from "../../context";
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const RegisterPage = () => {
+  const router = useRouter();
+  const { registerUser } = useContext(AuthContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onRegisterForm = async ({ name, email, password }: FormData) => {
+    setShowError(false);
+
+    const { hasError, message } = await registerUser(name, email, password);
+
+    if (hasError) {
+      setShowError(true);
+      setErrorMessage(message || "");
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+      return;
+    }
+
+    router.replace("/");
+  };
+
   return (
     <AuthLayout title={"Registrar"}>
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Crear cuenta
-            </Typography>
-          </Grid>
+      <form onSubmit={handleSubmit(onRegisterForm)}>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Crear cuenta
+              </Typography>
 
-          <Grid item xs={12}>
-            <TextField label="Nombre completo" variant="filled" fullWidth />
+              {showError && (
+                <Chip
+                  label={errorMessage}
+                  color="error"
+                  icon={<ErrorOutline />}
+                  className="fadeIn"
+                />
+              )}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Nombre completo"
+                variant="filled"
+                fullWidth
+                {...register("name", {
+                  required: "El nombre es obligatorio",
+                  minLength: {
+                    value: 2,
+                    message: "El nombre debe tener al menos 2 caracteres",
+                  },
+                })}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Correo"
+                type="email"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "El email es obligatorio",
+                  validate: validations.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contaseña"
+                variant="filled"
+                type="password"
+                fullWidth
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                  minLength: {
+                    value: 6,
+                    message: "La contraseña debe tener al menos 6 caracteres",
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                color="secondary"
+                className="circular-btn"
+                type="submit"
+                size="large"
+                fullWidth
+              >
+                Registrarse
+              </Button>
+            </Grid>
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href={`/auth/login`} passHref>
+                <Link underline="always">¿Ya tienes cuenta?</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              label="Contaseña"
-              variant="filled"
-              type="password"
-              fullWidth
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Registrarse
-            </Button>
-          </Grid>
-
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href={`/auth/login`} passHref>
-              <Link underline="always">¿Ya tienes cuenta?</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
